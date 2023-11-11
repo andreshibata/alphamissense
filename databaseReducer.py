4be13a7aa466b648966bc1c9d54c2c37eddd57d0
andreshibata <41885690+andreshibata@users.noreply.github.com>
2023-11-11 15:34:23 -0500

    Added usage of iterparse

--- databaseReducer.py
+++ databaseReducer.py
@@ -6,28 +6,39 @@ def reduce_clinvar_xml_size(file_name, reduction_factor=10):
     # Construct the full file path
     file_path = os.path.join(os.getcwd(), file_name)
 
-    # Parse the XML file and get the root
-    tree = ET.parse(file_path)
-    root = tree.getroot()
+    # Get the total file size for progress estimation
+    total_file_size = os.path.getsize(file_path)
+
+    # Initialize the progress bar
+    pbar = tqdm(total=total_file_size, desc='Processing ClinVarSets')
+
+    # Count the total number of ClinVarSet elements
+    total_clinvar_sets = sum(1 for event, elem in ET.iterparse(file_path, events=('end',)) if elem.tag.endswith('ClinVarSet'))
 
     # Calculate the number of ClinVarSets to retain
-    total_clinvar_sets = len(root.findall('.//ClinVarSet'))
     number_to_retain = total_clinvar_sets // reduction_factor
 
-    # Create a list of ClinVarSet elements to be removed
-    clinvar_sets_to_remove = []
+    # Reset the iterator for actual parsing
+    context = ET.iterparse(file_path, events=('start', 'end'))
+    _, root = next(context)  # get root element
 
-    # Select ClinVarSets to be removed
-    for i, clinvar_set in enumerate(root.findall('.//ClinVarSet')):
-        if i % reduction_factor != 0:
-            clinvar_sets_to_remove.append(clinvar_set)
+    removed_count = 0
+    for event, elem in context:
+        if event == 'end' and elem.tag.endswith('ClinVarSet'):
+            if removed_count < (total_clinvar_sets - number_to_retain):
+                root.remove(elem)
+                removed_count += 1
+            # Clear the processed elements from memory
+            elem.clear()
 
-    # Remove the selected ClinVarSets with a progress bar
-    for clinvar_set in tqdm(clinvar_sets_to_remove, desc='Reducing file size'):
-        root.remove(clinvar_set)
+        # Update the progress bar based on the file's current position
+        pbar.update(os.path.getsize(file_path) - pbar.n)
+
+    pbar.close()
 
     # Write the reduced XML back to a new file
     reduced_file_name = f'reduced_{file_name}'
+    tree = ET.ElementTree(root)
     tree.write(os.path.join(os.getcwd(), reduced_file_name))
 
     print(f'Reduced file written to {reduced_file_name}')
